5726e1b3ae13c5fe61bb75b815493c0ba819a5b3
GurlFriday88 <entrotlek@gmail.com>
2017-12-22 16:31:04 -0500

    completed web-caesar

--- main.py
+++ main.py
@@ -6,57 +6,56 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-formOne= """
+
+form= """
 <!DOCTYPE html>
+
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-                display: block;
-            }
+            }}
         </style>
     </head>
     <body>
-     
-        <form action = "/" name= "form" method= "POST">
-            <label for = "rot"> 
-            Rotate By 
-                <input type = "text"  name = "rot" placeholder= 0>
+        <form action="/" method="post">
+            <label>
+                Rotate By
+                <input type="text" name="rot" value= "0"/>     
             </label>
-                <textarea name= "text"></textarea>
-            <input type= "submit" value="encrypt"/>
 
-        </form>
+            <textarea name="text" >{0}</textarea>
+
+            <input type="submit" value="Encrypt It"/>
+         </form>
+
     </body>
 </html>
-
 
 """
 
 
 @app.route('/')
 def index():
-    content = formOne
-    return content
+    return form.format("")
 
 @app.route('/', methods=["POST"])
 def encrypt():
     rotation_element = int(request.form['rot'])
     text_element= request.form['text']
-    encryption_element =rotate_string(text_element, rotation_element)
-    content = "<h1>"+ encryption_element + "</h1>"
-    return content
+    result = rotate_string(text_element, rotation_element)
+    return form.format(result)
 
 
 app.run()
